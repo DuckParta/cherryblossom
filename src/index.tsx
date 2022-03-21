8bc7efdacd14f9333c2e1cd9ecd65079d4b9f0ec
HJ <eowr22@gmail.com>
2022-03-21 21:55:15 +0900

    chore merge conflict

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './app/App';
 
 import { Provider } from "react-redux";
 import { Action, ThunkDispatch, configureStore } from '@reduxjs/toolkit';
@@ -14,9 +12,7 @@ const store = configureStore({
 
 export type ThunkAppDispatch = ThunkDispatch<RootState, void, Action>;
 export const useAppThunkDispatch = () => useDispatch<ThunkAppDispatch>();
-=======
 import App from './App';
->>>>>>> b94364decf7057bfa97469e4273fd70ce577543d
 
 ReactDOM.render(
   <Provider store={store}>
