6bc8ea4d8b7f5643a4a867b5fb35939880bc84fd
GAEUN SHIN <newsilver1028@naver.com>
2022-03-22 14:37:45 +0900

    feat: Add FestivalItem

--- src/common/Container.tsx
+++ src/common/Container.tsx
@@ -1,38 +1,8 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { ReactElement, useEffect } from 'react';
-
-import { RootState } from '../features/reducers';
-import { fetchFestivalData } from '../features/async/fetchFestivalData';
-import { Items } from './festivalDataInterface';
-import FestivalsList from './FestivalsList';
-
+import FestivalsList from "./FestivalsList";
 
 export default function Container() {
-  const dispatch = useDispatch();
-  const { body, isLoading } = useSelector((store: RootState) => store.festivalDataReducer);
-  const getData = async () => {
-    await dispatch(fetchFestivalData());
-  }
-  console.log(body)
-  const items = body.items;
-  const itemsRender = items?.map((item: Items, index): JSX.Element => {
-    return (
-    <FestivalsList key={item.fstvlNm + JSON.stringify(index)} item={item}/>
-    );
-  });
   
-  useEffect(() => {
-    getData();
-  },[]);
-
-  // console.log(body);
-
   return (
-    <>
-    <div>{isLoading && "Loading..."}</div>
-    <div>
-      {itemsRender}
-    </div>
-    </>
+    <FestivalsList />
   ) 
 }
--- src/common/FestivalsList.tsx
+++ src/common/FestivalsList.tsx
@@ -1,15 +1,42 @@
-import { Items } from "./festivalDataInterface";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+
+import { RootState } from '../features/reducers';
+import { fetchFestivalData } from '../features/async/fetchFestivalData';
+import { Items } from './festivalDataInterface';
+import FestivalItem from './FestivalItem';
 
 export default function FestivalsList(props: any) {
-  const item = props.item;
+  const targetRef = useRef(null);
+  const dispatch = useDispatch();
+  const { body, isLoading } = useSelector((store: RootState) => store.festivalDataReducer);
+  const getData = async () => {
+    await dispatch(fetchFestivalData());
+  }
+  // console.log(body);
+
+  const items = body.items;
+  const itemsRender = items?.map((item: Items, index): JSX.Element => {
+    return (
+    <FestivalItem key={item.fstvlNm + JSON.stringify(index)} item={item}/>
+    );
+  });
+  
+  useEffect(() => {
+    getData();
+  },[dispatch]);
+
+  console.log(body);
+
   return (
     <>
     <div>
-      <div>{item.fstvlNm}</div>
-      <div>{item.auspcInstt}</div>
-      <div>{item.opar}</div>
-      <div>{item.fstvlStartDate}</div>
-      </div>
+    <div>{isLoading && "Loading..."}</div>
+    <div>
+      {itemsRender}
+    </div>
+    <div ref={targetRef}></div>
+    </div>
     </>
   );
 }
