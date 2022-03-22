192b43fbc19b83318631d6f7b149e80f193f7980
HJ <eowr22@gmail.com>
2022-03-22 15:11:39 +0900

    chore: revert App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,14 @@ import Container from "../common/Container";
 import { ChakraProvider } from "@chakra-ui/react";
 import MainHeader from "../common/MainHeader";
 import Appbar from "../common/Appbar";
-import FestivalContents from "../common/FestivalContents";
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <FestivalContents />
+        <Appbar />
+        <MainHeader />
+        <Container />
       </div>
     </ChakraProvider>
   );
