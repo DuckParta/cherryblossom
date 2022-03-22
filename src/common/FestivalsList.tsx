9b23dcb7b230cf137bda4aa06c6535a87ed87836
GAEUN SHIN <newsilver1028@naver.com>
2022-03-22 16:11:03 +0900

    feat: Add custom hook useIntersectionObserver

--- src/common/FestivalsList.tsx
+++ src/common/FestivalsList.tsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { RootState } from '../features/reducers';
 import { fetchFestivalData } from '../features/async/fetchFestivalData';
 import { Items } from './festivalDataInterface';
 import FestivalItem from './FestivalItem';
+import useIntersectionObserver from '../features/hooks/useIntersectionObserver';
 
 export default function FestivalsList(props: any) {
   const targetRef = useRef(null);
@@ -22,20 +23,55 @@ export default function FestivalsList(props: any) {
     );
   });
   
-  useEffect(() => {
-    getData();
-  },[dispatch]);
+  // useEffect(() => {
+  //   getData();
+  // },[dispatch]);
 
   console.log(body);
+
+  // useIntersectionObserver
+  const [page, setPage] = useState(1);
+  const { loading, error, list } = useIntersectionObserver(page);
+  const loader = useRef(null);
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setPage((prev) => prev + 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    // getData();
+    const option = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 0
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    if (loader.current) observer.observe(loader.current);
+  },[handleObserver]);
+
+  const renderList = list.map((item: Items, index: number): JSX.Element => {
+    const isLastElement = list.length === index + 1;
+    return (
+      <div>{index}
+      <FestivalItem key={item.fstvlNm + JSON.stringify(index)} item={item}/>
+      {isLastElement && <FestivalItem key={item.fstvlNm + JSON.stringify(index)} item={item}/>}
+      {loading && <h1>io loading...</h1>}
+      <div ref={loader}></div>
+      </div>
+    )
+  })
 
   return (
     <>
     <div>
-    <div>{isLoading && "Loading..."}</div>
-    <div>
-      {itemsRender}
-    </div>
-    <div ref={targetRef}></div>
+      <div>=== IntersectionObserver ===</div>
+      {loading && <div><b>io loading...</b></div>}
+      <div>
+        {/* {itemsRender} */}
+        {renderList}
+      </div>
     </div>
     </>
   );
--- src/features/hooks/useIntersectionObserver.tsx
+++ src/features/hooks/useIntersectionObserver.tsx
@@ -1,5 +1,28 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import axios from 'axios';
 
-// export default function useIntersectionObserver({ root, target, onIntersect, threshold = 1.0, rootMargin = "0px" }) {
+export default function useIntersectionObserver(page: any) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<unknown>(false);
+  const [list, setList] = useState<any>([]);
 
-// }
+  const sendQuery = useCallback(async () => {
+    try {
+      await setLoading(true);
+      await setError(false);
+      const response = await axios.get("http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api?serviceKey=PsnPqBdiFYqwLlJF6wAm8TjrIHmfHqIpRoH0Pch%2B8%2FYdNtxltESW1eKpCM1RvH3nbTXwl7JFWQE8bdKNnuPtag%3D%3D&pageNo=0&numOfRows=300&type=json")
+      // return response.data.response;
+      const data = response.data.response.body.items;
+      await setList((prev: any) => [...prev, ...data]);
+      setLoading(false);
+    } catch(error) {
+      setError(error);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    sendQuery();
+  }, [sendQuery, page]);
+
+  return { loading, error, list };
+}
