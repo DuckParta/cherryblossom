e1fd566299fc70370348916fa8f589f8205bcc72
HJ <eowr22@gmail.com>
2022-03-23 18:18:03 +0900

    feat: Add a logout button and handle logout

--- src/common/Appbar.tsx
+++ src/common/Appbar.tsx
@@ -13,7 +13,7 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import styled from "styled-components";
-import GoogleLogin from "react-google-login";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
 import { useState } from "react";
 
 const GoogleBtn = styled.button`
@@ -45,85 +45,40 @@ const KakaoBtn = styled.button`
 
 function Appbar() {
   interface IUser {
+    googleId: string;
     email: string;
     name: string;
   }
 
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [login, setLogin] = useState(true);
-  const [info, setInfo] = useState<IUser>();
+  const [login, setLogin] = useState(false);
+  const [userInfo, setUserInfo] = useState<IUser>({
+    googleId: "",
+    email: "",
+    name: "",
+  });
 
-  const onGoogleLogin = (email: string, name: string) => {
-    setInfo({ email, name });
-    setLogin(false);
+  const onGoogleLogin = (googleId: string, email: string, name: string) => {
+    setUserInfo({ googleId, email, name });
+    setLogin(true);
+    onClose();
   };
 
-  return (
-    <>
-      <Flex justifyContent="center">
-        <Box w="55em">
-          <Image htmlWidth="100px" src="../images/logo.png" />
-        </Box>
-        {login ? (
-          <Center w="7em">
-            <Button onClick={onOpen} colorScheme="teal">
-              로그인
-            </Button>
-          </Center>
-        ) : (
-          `안녕하세요 ${info?.name}님`
-        )}
-      </Flex>
-      {login ? (
-        <Modal
-          blockScrollOnMount={true}
-          size="md"
-          isCentered
-          motionPreset="slideInBottom"
-          isOpen={isOpen}
-          trapFocus={false}
-          onClose={onClose}
-        >
-          <ModalOverlay>
-            <ModalContent>
-              <ModalHeader>
-                <Center>Cherry Blossom에 오신 걸 환영합니다!</Center>
-              </ModalHeader>
-              <ModalBody>
-                <Flex flexDirection="column">
-                  <GoogleLoginBtn onGoogleLogin={onGoogleLogin} />
-                  <KakaoBtn>Kakao 로그인</KakaoBtn>
-                </Flex>
-              </ModalBody>
-              <ModalFooter>
-                <Button colorScheme="gray" mr={3} onClick={onClose}>
-                  Close
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </ModalOverlay>
-        </Modal>
-      ) : null}
-    </>
-  );
-}
-
-function GoogleLoginBtn({ onGoogleLogin }: any) {
   const clientId = process.env.REACT_APP_GOOGLE_LOGIN_API || "";
 
-  const onSuccess = async (response: any) => {
-    const {
-      profileObj: { email, name },
-    } = response;
-    await onGoogleLogin(email, name);
-  };
+  function GoogleLoginBtn({ onGoogleLogin }: any) {
+    const onSuccess = async (response: any) => {
+      const {
+        profileObj: { googleId, email, name },
+      } = response;
+      await onGoogleLogin(googleId, email, name);
+    };
 
-  const onFailure = (error: any) => {
-    console.log(error);
-  };
+    const onFailure = (error: any) => {
+      console.log(error);
+    };
 
-  return (
-    <div>
+    return (
       <GoogleLogin
         clientId={clientId}
         responseType={"id_token"}
@@ -138,7 +93,81 @@ function GoogleLoginBtn({ onGoogleLogin }: any) {
         onSuccess={onSuccess}
         onFailure={onFailure}
       />
-    </div>
+    );
+  }
+
+  function GoogleLogoutBtn() {
+    const logout = () => {
+      setLogin(false);
+      setUserInfo({ googleId: "", email: "", name: "" });
+    };
+
+    return (
+      <GoogleLogout
+        clientId={clientId}
+        onLogoutSuccess={logout}
+        render={(renderProps) => (
+          <Center w="7em">
+            <Button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              로그아웃
+            </Button>
+          </Center>
+        )}
+      />
+    );
+  }
+
+  return (
+    <>
+      <Flex justifyContent="center">
+        <Box w="55em">
+          <Image htmlWidth="100px" src="../images/logo.png" />
+        </Box>
+        {login ? (
+          <>
+            안녕하세요 {userInfo?.name}님
+            <GoogleLogoutBtn />
+          </>
+        ) : (
+          <Center w="7em">
+            <Button onClick={onOpen} colorScheme="teal">
+              로그인
+            </Button>
+          </Center>
+        )}
+      </Flex>
+      <Modal
+        blockScrollOnMount={true}
+        size="md"
+        isCentered
+        motionPreset="slideInBottom"
+        isOpen={isOpen}
+        trapFocus={false}
+        onClose={onClose}
+      >
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>
+              <Center>Cherry Blossom에 오신 걸 환영합니다!</Center>
+            </ModalHeader>
+            <ModalBody>
+              <Flex flexDirection="column">
+                <GoogleLoginBtn onGoogleLogin={onGoogleLogin} />
+                <KakaoBtn>Kakao 로그인</KakaoBtn>
+              </Flex>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="gray" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </>
   );
 }
 
