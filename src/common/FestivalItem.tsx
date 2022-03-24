196f8048b0446b743f63da4cc9fc6cf1dd5dd86a
GAEUN SHIN <newsilver1028@naver.com>
2022-03-24 15:50:06 +0900

    fix: Fix typo and Add festivalItem click handler

--- src/common/FestivalItem.tsx
+++ src/common/FestivalItem.tsx
@@ -1,9 +1,14 @@
 export default function FestivalItem(props: any) {
   const item = props.item;
+  const { fstvlNm } = props.item;
+
+  function handleFestivalListClick() {
+    console.log(fstvlNm);
+  }
 
   return (
     <>
-    <div>
+    <div onClick={handleFestivalListClick}>
       <div>{item.fstvlNm}</div>
       <div>{item.auspcInstt}</div>
       <div>{item.opar}</div>
