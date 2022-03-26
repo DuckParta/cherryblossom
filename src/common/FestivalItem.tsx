65c2018456e0ea88fbdb962edbe9949cad6588b4
GAEUN SHIN <newsilver1028@naver.com>
2022-03-27 01:21:18 +0900

    style: Add Skeleton style to FestivalItem

--- src/common/FestivalItem.tsx
+++ src/common/FestivalItem.tsx
@@ -1,28 +1,51 @@
 import { Items } from "./festivalDataInterface";
 
+import { Box, Flex, Text, Heading, Center, Image } from "@chakra-ui/react";
+
 export default function FestivalItem(props: { items: Items }) {
-  const { fstvlNm, auspcInstt, opar, fstvlStartDate, decimalDay } = props.items;
+  const { fstvlNm, opar, fstvlStartDate, fstvlEndDate, decimalDay } = props.items;
 
   function handleFestivalListClick() {
     console.log(fstvlNm);
   }
 
   function handleWishButtonClick() {
-    console.log(fstvlNm);
+    console.log(fstvlNm + " add wish list");
   }
 
   return (
-    <>
-    <div>
-      <div onClick={handleFestivalListClick}>
-        <div>{fstvlNm}</div>
-        <div>{auspcInstt}</div>
-        <div>{opar}</div>
-        <div>{fstvlStartDate}</div>
-        <div>{decimalDay}</div>
-      </div>
-      <button onClick={handleWishButtonClick}>찜하기</button>
-    </div>
-    </>
+    <Flex
+    flexFlow="column nowrap"
+    h="100%"
+    >
+      <Box onClick={handleFestivalListClick}
+      h="70%"
+      cursor="pointer">
+        <Heading fontSize="lg"
+          mt="50px"
+          overflow="hidden"
+          >{fstvlNm}</Heading>
+      </Box>
+      <Center
+      h="20%"
+      >
+        <Heading 
+          padding="5px 10px"
+          margin="10px"
+          borderRadius="lg"
+          bgColor="gray.100"
+          fontSize="lg">
+            {decimalDay}
+          </Heading>
+          <Image src={`${process.env.PUBLIC_URL}/images/wish_icon.png`}
+          w="30px"
+          alt="wish"
+          onClick={handleWishButtonClick} 
+          cursor="pointer"/>
+      </Center>
+      
+      <Text my="5px">{fstvlStartDate} ~ {fstvlEndDate}</Text>
+      <Text>{opar}</Text>
+    </Flex>
   );
 }
