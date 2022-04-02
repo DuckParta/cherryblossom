e550b6bb10ebdf37564a7c3534a4f7fcc78b65e3
GAEUN SHIN <newsilver1028@naver.com>
2022-04-02 23:59:28 +0900

    fix: Framer Motion v4 breaks Chakra issue #3618

--- src/common/service/firebase.ts
+++ src/common/service/firebase.ts
@@ -1,20 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import firebase from "firebase-admin";
 import { getFirestore } from "firebase/firestore";
-// import 'firebase/compat/firestore';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDnz3_iswfbpZ7IytHcLtdLYZg6cp4xzl0",
   authDomain: "cherry-blossom-7a823.firebaseapp.com",
   databaseURL: "https://cherry-blossom-7a823-default-rtdb.firebaseio.com",
   projectId: "cherry-blossom-7a823",
-};
+  storageBucket: "cherry-blossom-7a823.appspot.com",
+  messagingSenderId: "799184670217",
+  appId: "1:799184670217:web:f85ab3a88074d8f6f972d8",
+  measurementId: "G-4R4V04S7D0"
+}
+// Initialize Firebase
 
-firebase.initializeApp(firebaseConfig);
 export const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
-export const firestore = firebase.firestore();
 export const db = getFirestore();
 
 // const firebaseConfig = {
