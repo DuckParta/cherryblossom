382600d0376b8bbfa0b756ba794a20f5a864b454
HJ <eowr22@gmail.com>
2022-04-12 17:32:46 +0900

    refactor: Fix nested components

--- src/features/UI/WishList.tsx
+++ src/features/UI/WishList.tsx
@@ -28,76 +28,37 @@ function WishList() {
 
   function getFestival() {
     const userRef = ref(database, `${user.userId}`);
-    onValue(userRef, (snapshot) => {
-      const data = snapshot.val();
-      let fstvlList: Items[];
+    onValue(userRef, async (snapshot) => {
+      const data = await snapshot.val();
+      let fetchFestival: Items[];
 
       if (data !== null) {
-        fstvlList = Object.values(data);
+        fetchFestival = Object.values(data);
         setDeleteKeys(() => Object.keys(data));
       } else {
         setDeleteKeys([]);
       }
-      setFestivalList(fstvlList!);
+      setFestivalList(fetchFestival!);
     });
   }
-
-  const renderFestivalList = (list: any) => {
-    if(!list) {
-      return (
-        <Box h="container.lg" textAlign="center" my="10%">
-          <Heading mb="5%" textAlign="center" fontSize="5xl" fontFamily="Courgette" textColor="GrayText">
-            Cherry Blossom
-          </Heading>
-          <Heading size="md" textColor="GrayText" fontWeight="semibold">아직 찜한 축제가 없습니다.</Heading>
-        </Box>
-      )
-    }
-    return (
-      <>
-        {list.map((item: Items, index: number): JSX.Element => {
-          const itemKey = item.fstvlId;
-          if (item.isPassedDate) {
-            return (
-              <Box key={itemKey}>
-                <OutOfDateFestivalItem items={item} />
-                <Center>
-                  <Button onClick={() => onDelete(index)}>삭제</Button>
-                </Center>
-              </Box>
-            );
-          }
-          return (
-            <Box key={itemKey}>
-              <Link to={`/${item.fstvlId}`} key={itemKey}>
-                <FestivalItem items={item} />
-              </Link>
-              <Center>
-                <Button onClick={() => onDelete(index)}>삭제</Button>
-              </Center>
-            </Box>
-          );
-        })}
-      </>
-    );
-  } 
 
   return (
     <Box>
       <AppBar />
-        <Box pb="20%" bgColor="gray.50">
+      <Box pb="20%" bgColor="gray.50">
         <Center py="10%">
-          <Image 
-          src={`${process.env.PUBLIC_URL}/images/wish_active_icon.png`} 
-          w="25px"
-          mx="10px"/>
+          <Image
+            src={`${process.env.PUBLIC_URL}/images/wish_active_icon.png`}
+            w="25px"
+            mx="10px"
+          />
           <Heading>내 축제 리스트</Heading>
         </Center>
         <Center>
           <Box width="70%">
             <Flex flexFlow="column wrap" justifyContent="space-around">
               <Flex flexFlow="row wrap" justifyContent="space-around">
-                {renderFestivalList(festivalList)}
+                {renderFestivalList(festivalList, onDelete)}
               </Flex>
             </Flex>
           </Box>
@@ -106,5 +67,55 @@ function WishList() {
     </Box>
   );
 }
+
+const renderFestivalList = (list: any, onDelete: any) => {
+  return (
+    <>
+      {!list ? (
+        <Box h="container.lg" textAlign="center" my="10%">
+          <Heading
+            mb="5%"
+            textAlign="center"
+            fontSize="5xl"
+            fontFamily="Courgette"
+            textColor="GrayText"
+          >
+            Cherry Blossom
+          </Heading>
+          <Heading size="md" textColor="GrayText" fontWeight="semibold">
+            아직 찜한 축제가 없습니다.
+          </Heading>
+        </Box>
+      ) : (
+        <>
+          {list.map((item: Items, index: number): JSX.Element => {
+            const itemKey = item.fstvlId;
+            if (item.isPassedDate) {
+              return (
+                <Box key={itemKey}>
+                  <OutOfDateFestivalItem items={item} />
+                  <Center>
+                    <Button onClick={() => onDelete(index)}>삭제</Button>
+                  </Center>
+                </Box>
+              );
+            } else {
+              return (
+                <Box key={itemKey}>
+                  <Link to={`/${item.fstvlId}`} key={itemKey}>
+                    <FestivalItem items={item} />
+                  </Link>
+                  <Center>
+                    <Button onClick={() => onDelete(index)}>삭제</Button>
+                  </Center>
+                </Box>
+              );
+            }
+          })}
+        </>
+      )}
+    </>
+  );
+};
 
 export default WishList;
